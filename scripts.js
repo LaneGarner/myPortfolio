efb51659a8ba7cc65f072fc1238b96ea3afe0c03
Lane Garner <b.lane.garner@gmail.com>
2020-09-30 17:46:27 -0500

    photo gallery page first commit

--- scripts.js
+++ scripts.js
@@ -5,3 +5,10 @@ new URLSearchParams(window.location.search).forEach((value, name) => {
   placeForResults.append(`${name} : ${value}`)
   placeForResults.append(document.createElement("br"))
 })
+
+
+document.getElementById("hoverMe").addEventListener("mouseover", function() {
+
+  this.classList.add('animate__rubberBand');
+
+});
